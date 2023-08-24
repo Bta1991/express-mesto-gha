83efb39ba1c19ec466d21c6d32f31b5465ed0acb
tanyaka <litium@yandex.ru>
2023-08-24 14:50:19 +0300

    0.9.5 изменен порядок роутов

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,19 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users'); //  путь к контроллеру пользователей
 
-// GET / — возвращает всех пользователей
-router.get('/', userController.getAllUsers);
+// GET /me - возвращает информацию о текущем пользователе
+router.get('/me', userController.getCurrentUser);
 
 // GET /:userId - возвращает пользователя по _id
 router.get('/:userId', userController.getUserById);
-
-// GET /me - возвращает информацию о текущем пользователе
-router.get('/me', userController.getCurrentUser);
 
 // PATCH /me — обновляет профиль
 router.patch('/me', userController.updateUserProfile);
 
 // PATCH /me/avatar — обновляет аватар
 router.patch('/me/avatar', userController.updateUserAvatar);
+
+// GET / — возвращает всех пользователей
+router.get('/', userController.getAllUsers);
 
 module.exports = router;
