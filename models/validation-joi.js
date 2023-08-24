1ca37543930d4774a2833de038b28825ffe61f68
tanyaka <litium@yandex.ru>
2023-08-24 12:13:43 +0300

    испрвавлена схема joi для пользователя

--- models/validation-joi.js
+++ models/validation-joi.js
@@ -4,5 +4,8 @@ module.exports.signupValidationSchema = {
   body: Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
   }),
 };
